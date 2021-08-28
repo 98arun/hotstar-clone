e6101dbf58cf3ac8130b3e0027d775d958936aa1
Arun <82587103+98arun@users.noreply.github.com>
2021-08-28 18:20:49 +0530

    add popup

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,11 @@
-console.log('hey');
+console.log("hey");
+let loginbtn = document.getElementById("loginbtn");
+let closebtn = document.getElementById("close");
+let popup = document.getElementById("pop-up");
+
+loginbtn.addEventListener("click", () => {
+  popup.style.display = "block";
+});
+closebtn.addEventListener("click", () => {
+  popup.style.display = "none";
+});
